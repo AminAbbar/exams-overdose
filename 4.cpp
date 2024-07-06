b52c37b054fe0d8eff7fb3e3ee20b5c7ea3bdc78
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-07-07 01:04:30 +0200

    4, 3 ,6

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   int a[5]; int  n=5 , i ,j,f=0;
+
+   //a[5] = {10, 20,30,40,50};  ///  خطأ بيخلي الكود كله ميخدمش الجواب لايطبع شيء
+   
+
+    return 0;
+}
---
+++ 5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+         
+     // int a = {4,6,6,3,33,24}; ///  خطأ بيخلي الكود كله ميخدمش الجواب لايطبع شيء
+
+    return 0;
+}
