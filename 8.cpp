84bf081caabaa63c93a6193d56466e72215736f0
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-07-07 01:18:40 +0200

    7 8 9 10

---
+++ 8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+      int i = 1, j=1,t=6,n=7;
+
+      t=++i+t;
+      i=i+t;
+      j+=n;
+      if(t+t >= 12 && j+j>=15)
+        cout << t << i << j << n;
+      else
+       cout<<t;
+      
+
+
+    return 0;
+}
