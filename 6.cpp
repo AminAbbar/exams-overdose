759fce7c99d6673031d8523eb6571bbe5084f786
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-07-16 05:15:45 +0200

    .

--- 6.cpp
+++ 6.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main(){
-
+    int sum = 0;
     for (int i = 101; i < 1000 ; i++){
         if (i % 5 == 0) { // لو قبل القسمة عليه يعني من مضاعفاته
-            cout << i << endl;
+            sum += i;
         }
     }    
+    cout << sum << endl;
 
     return 0;
 }
--- 9.cpp
+++ 9.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main(){
 
-
+    int sum = 0;
     for (int i = 1001; i < 10000 ; i++){
         if (i % 9 == 0) { // لو قبل القسمة عليه يعني من مضاعفاته
-            cout << i << endl;
+            sum += i;
         }
     } 
+    cout << sum << endl;
 
 
     return 0;
