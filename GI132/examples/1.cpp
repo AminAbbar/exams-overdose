719c70f44fd3c90a671768a64b8f8b9e9b98410b
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-10-12 18:32:02 +0200

    Update 1.cpp

--- GI132/examples/1.cpp
+++ GI132/examples/1.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+
+void getArrayValues(int n , int array[]){
+
+   for(int i = 0 ; i < n ; i++)
+       cin >> array[i];
+}
+
+
+int findXLocation(int array[]){
+    
+}
+
+
+
 int main(){
       
+    int n ;
+    int array[10000];
+    cin >> n;
+    getArrayValues(n , array);
+
 
       
 
