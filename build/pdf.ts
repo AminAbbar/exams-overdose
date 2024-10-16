108d6bc55efada76c4d19b8ecfb12fb311b7989c
Amin <91750157+AminAbbar@users.noreply.github.com>
2024-10-16 23:00:41 +0200

    pdfooo

--- build/pdf.ts
+++ build/pdf.ts
@@ -1,72 +1,131 @@
-import { execSync } from "node:child_process"
-import { writeFileSync } from "node:fs";
-import puppeteer from "npm:puppeteer";
-import { join, basename } from "@std/path";
+import * as path from "@std/path";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+import { PDFDocument } from 'npm:pdf-lib';
+
+const decoder = new TextDecoder();
+const metaData: Record<string, string> = JSON.parse(decoder.decode(Deno.readFileSync("./metadata.json")));
+const dirname = import.meta.dirname;
+
+const toIgnore = ['.git', '.vscode', 'build'];
+
+const getFolderContent = (dir: string, Directories = true) => {
+  if (!dirname) throw new Error("Failed to get subjects");
+
+  return Array.from(Deno.readDirSync(dir))
+    .filter(file => file.isDirectory == Directories && !toIgnore.includes(file.name));
+};
+
+const hasFolder = (files: Deno.DirEntry[], folderName: string) => {
+  return files.some(file => file.isDirectory && file.name === folderName);
+};
+
+
 
 const generateCodeSnippet = (cppFilePath: string, outputDir: string): string => {
-  const fileNameWithoutExt = basename(cppFilePath, ".cpp");
-  const outputFilePath = join(outputDir, `${fileNameWithoutExt}.png`);
+  const fileNameWithoutExt = path.basename(cppFilePath, ".cpp");
+  const tempName = `${fileNameWithoutExt}_${Date.now()}`
+  const outputFilePath = path.join(outputDir, `${tempName}.png`);
+  execSync(`carbon-now ${cppFilePath} --save-to=${outputDir} --save-as=${tempName} --theme dracula`);
 
-  execSync(`carbon-now ${cppFilePath} --save-to=${outputDir} --save-as=${fileNameWithoutExt} --theme dracula`);
-
-  if (!Deno.statSync(outputFilePath).isFile) {
+  if (!fs.existsSync(outputFilePath)) {
     throw new Error(`Snippet generation failed for: ${cppFilePath}`);
   }
 
   return outputFilePath;
 };
 
-const generateHtmlFile = async (htmlFilePath: string, questionImagePath: string, codeSnippetImagePath: string) => {
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Question and Solution</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                margin: 0;
-                padding: 0;
-            }
-            .question {
-                margin-bottom: 20px;
-            }
-            .solution {
-                margin-top: 20px;
-            }
-            img {
-                max-width: 100%;
-                height: auto;
-            }
-        </style>
-    </head>
-    <body>
-            <img src="../${questionImagePath}" alt="Question Image">
-            <img src="../${codeSnippetImagePath}" alt="Code Snippet">
-    </body>
-    </html>
-  `;
+const generatePdf = async (pdfDoc:PDFDocument , questionImagePath: string, codeSnippetImagePath: string) => {
+  const questionImageBytes = fs.readFileSync(questionImagePath);
+  const codeSnippetImageBytes = fs.readFileSync(codeSnippetImagePath);
 
-   writeFileSync(htmlFilePath, htmlContent);
-  console.log(`HTML file generated at ${htmlFilePath}`);
+ 
+
+  const page = pdfDoc.addPage();
+
+  const questionImage = await pdfDoc.embedPng(questionImageBytes);
+  const codeSnippetImage = await pdfDoc.embedPng(codeSnippetImageBytes);
+
+  const codeSnippetDims = codeSnippetImage.scale(0.5);
+  const questionDims = questionImage.scale(1);
+  // page.setSize(Math.max(questionDims.width, codeSnippetDims.width), questionDims.height + codeSnippetDims.height);
+  page.setSize(codeSnippetDims.width, questionDims.height + codeSnippetDims.height);
+
+  page.drawImage(questionImage, {
+    x: 0,
+    y: codeSnippetDims.height,
+    width: codeSnippetDims.width,
+    height: questionDims.height,
+  });
+
+  page.drawImage(codeSnippetImage, {
+    x: 0,
+    y: 0,
+    width: codeSnippetDims.width,
+    height: codeSnippetDims.height,
+  });
+
+
 };
 
-const generatePdf = async (codeSnippetImagePath: string, outputPdfPath: string) => {
+const generateSubjectPdfs = async () => {
+  const subjects = getFolderContent(path.join(dirname as string, ".."));
 
-  console.log(`PDF generated at ${outputPdfPath}`);
+  for (const subject of subjects) {
+    const subjectPath = path.join(dirname as string, "..", subject.name);
+    const midFolder = hasFolder(Array.from(Deno.readDirSync(subjectPath)), "mid");
+    const finalFolder = hasFolder(Array.from(Deno.readDirSync(subjectPath)), "final");
+
+    if (midFolder) {
+      await processFolder(subjectPath, 'mid');
+    }
+    if (finalFolder) {
+      await processFolder(subjectPath, 'final');
+    }
+  }
 };
 
-const generateQuestionPdf = async ( questionImage: string, cppFile: string) => {
+const processFolder = async (subjectPath: string, period: string) => {
+  const traceDir = path.join(subjectPath, period, 'tracing');
+  const problemSolvingDir = path.join(subjectPath, period, 'problemSolving');
 
-  const codeSnippetImage = generateCodeSnippet(cppFile, outputDir);
+  const traceFiles = getFolderContent(traceDir, false).filter(file => file.name.endsWith(".cpp"));
+  if(traceFiles.length > 0) {
+  const tracePDF = await PDFDocument.create();
+  const tracPDFPath = path.join(subjectPath, `tracing.pdf`);
+  for (const file of traceFiles) {
+    const cppFilePath = path.join(traceDir, file.name);
+    const questionImagePath = path.join(traceDir, file.name.replace(".cpp", ".png"));
+  
+    const codeSnippetImage = generateCodeSnippet(cppFilePath, traceDir);
+    await generatePdf(tracePDF ,questionImagePath, codeSnippetImage);
+    fs.unlinkSync(codeSnippetImage)
+  }
+  const tracePDFBytes = await tracePDF.save();
+  fs.writeFileSync(tracPDFPath, tracePDFBytes);
 
-  await generatePdf(codeSnippetImage, questionImage);
+
+}
+
+
+const problemSolvingFiles = getFolderContent(problemSolvingDir, false).filter(file => file.name.endsWith(".cpp"));
+if(problemSolvingFiles.length > 0) {
+
+  const problemSolvingPDF = await PDFDocument.create();
+  const problemSolvingPDFPath = path.join(subjectPath, `problemSolving.pdf`);
+  for (const file of problemSolvingFiles) {
+    const cppFilePath = path.join(problemSolvingDir, file.name);
+    const questionImagePath = path.join(problemSolvingDir, file.name.replace(".cpp", ".png"));
+
+    const codeSnippetImage = generateCodeSnippet(cppFilePath, problemSolvingDir);
+    await generatePdf(problemSolvingPDF,questionImagePath, codeSnippetImage);
+    fs.unlinkSync(codeSnippetImage)
+ 
+  }
+  const problemSolvingPDFBytes = await problemSolvingPDF.save();
+  fs.writeFileSync(problemSolvingPDFPath, problemSolvingPDFBytes);
+
+}
 };
 
-const questionImagePath = "../GI132/examples/1.png"; 
-const cppFilePath = "../GI132/examples/1.cpp";
-const outputDir = "./";
-
-await generateQuestionPdf( questionImagePath, cppFilePath);
+await generateSubjectPdfs();
